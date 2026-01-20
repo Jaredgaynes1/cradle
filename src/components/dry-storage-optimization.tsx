ae52776940bfc50572e4b6178d698ebee7ea6f84
Kyle Berlind <kyberlind@gmail.com>
2026-01-20 11:28:34 -0500

    add width to marketing and loyalty

--- src/components/dry-storage-optimization.tsx
+++ src/components/dry-storage-optimization.tsx
@@ -18,15 +18,6 @@ export const DryStorageOptimization = () => {
   return (
     <div className="w-full justify-center items-center flex flex-col py-16 px-4 md:px-16 border-b border-gray-200 min-h-200">
       <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-        <div className="flex flex-col gap-8 md:w-full sm:text-center md:text-left p-8">
-          <div className=" text-4xl md:text-5xl font-bold mb-7">
-            {DRY_STORAGE_TITLE}
-          </div>
-          <div className="text-gray-700 text-2xl md:text-2xl leading-relaxed">
-            {DRY_STORAGE_SUBTITLE}
-          </div>
-        </div>
-
         <div className="w-full bg-blue-50 rounded-xl p-4">
           <Image
             src={"/images/dry_storage_optimization.png"}
@@ -35,6 +26,14 @@ export const DryStorageOptimization = () => {
             height={300}
             className="w-full h-auto"
           />
+        </div>
+        <div className="flex flex-col gap-8 md:w-full sm:text-center md:text-left p-8">
+          <div className=" text-4xl md:text-5xl font-bold mb-7">
+            {DRY_STORAGE_TITLE}
+          </div>
+          <div className="text-gray-700 text-2xl md:text-2xl leading-relaxed">
+            {DRY_STORAGE_SUBTITLE}
+          </div>
         </div>
       </div>
     </div>
